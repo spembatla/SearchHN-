68c6641ad94f8af3f37327b8addf161daee8e923
spembatla <srikanthpembatla1432@gmail.com>
2023-12-15 17:00:43 +0530

    layout fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import SearchHN from "./components/searchbar";
+import PostDetails from "./components/postdetail";
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<SearchHN />}
+          />
+          <Route path="/post/:objectID" element={<PostDetails />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
---
+++ src/components/searchbar.js
@@ -0,0 +1,64 @@
+
+import React, { useState, useEffect } from "react";
+import { Autocomplete, Input, InputAdornment } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+import "./search.css";
+
+const searchHN = async (query) => {
+    const response = await fetch(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+    const data = await response.json();
+    return data.hits;
+};
+
+const SearchHN = () => {
+    const top100Films = [];
+
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+
+
+    const search = async () => {
+        const data = await searchHN(query);
+        setResults(data);
+    };
+
+    useEffect(() => {
+        search();
+    }, []);
+
+    useEffect(() => {
+        if (query) {
+            search();
+        }
+    }, [query, search]);
+
+    const handleSearch = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
+    return (
+        <div className="search">
+            <Autocomplete
+                id="search-id"
+                freeSolo
+                options={top100Films.map((option) => option.title)}
+                renderInput={(params) => <Input {...params} onChange={handleSearch} placeholder="Type here to Search" label="Search" startAdornment={<InputAdornment position="start">
+                    <Search />
+                </InputAdornment>} />}
+            />
+
+            <ul className="items">
+                {results && results.map((result, index) => {
+                    return <li className="item" key={result.objectID} >
+                        <a href={`/post/${result.objectID}`}>{result.title}</a></li>
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default SearchHN;
